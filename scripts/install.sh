8f140342f0c33fd35863c542eceb5cee8ba952dc
Sebastian Hestsveen <sebashes@stud.ntnu.no>
2023-04-20 09:06:52 +0200

    script uppdate

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install --lts
-nvm install 16
 npm install -y
 npm update -y
