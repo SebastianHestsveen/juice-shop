67f850dde11a1dd6ba50e95187c3514ff553527c
Sebastian Hestsveen <sebashes@stud.ntnu.no>
2023-04-20 11:37:43 +0200

    script uppdate

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,5 @@ cd /home
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install --lts
-cd /opt/codedeploy-agent/deployment-root/844c77ae-32f8-4eb5-8816-dd95f2200718
-pwd
 sudo su
 echo "jegErSudo"
